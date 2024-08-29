3c5a49817afde3a74c2d5bd858515b410af50211
Gabriel-PachecoSS <gabera.pss@gmail.com>
2024-08-29 15:32:26 -0300

    return to studies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const clean = require('gulp-clean');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +9,19 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+function cleanDist() {
+    return gulp.src('./dist', { read: false, allowEmpty: true })
+        .pipe(clean());
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function () {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/images/**/*', gulp.series(images));
 }
