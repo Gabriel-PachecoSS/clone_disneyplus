ee23a5508195eb4465efef45ec619d1780ab97c5
Gabriel-PachecoSS <gabera.pss@gmail.com>
2024-08-29 15:56:15 -0300

    delete gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
-const clean = require('gulp-clean');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -15,13 +14,7 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-function cleanDist() {
-    return gulp.src('./dist', { read: false, allowEmpty: true })
-        .pipe(clean());
-}
-
 exports.default = gulp.parallel(styles, images);
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/images/**/*', gulp.series(images));
 }
